1a08ea4c9f3711367794f3e432482109c6a0ca05
teranerehimova <teranerhmv@gmail.com>
2023-11-03 19:20:27 +0400

    added value

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Routing from './Routing'
+import './App.css'
 
 const App = () => {
   return (
--- src/Components/HeaderTop/index.jsx
+++ src/Components/HeaderTop/index.jsx
@@ -3,6 +3,10 @@ import styles from './style.module.css'
 import { BsTelephoneFill } from 'react-icons/bs'
 import { AiOutlineMail } from 'react-icons/ai'
 import { ImLocation } from 'react-icons/im'
+import { FaFacebookF } from 'react-icons/fa'
+import { AiOutlineInstagram } from 'react-icons/ai'
+import { AiFillLinkedin } from 'react-icons/ai'
+import { AiOutlineWhatsApp } from 'react-icons/ai'
 
 const HeaderTop = () => {
   return (
@@ -11,21 +15,24 @@ const HeaderTop = () => {
         <div className={styles.contactAndSocial}>
           <div className={styles.contactInfo}>
             <div className='d-flex align-items-center'>
-              <BsTelephoneFill className='me-2'/> 
+              <BsTelephoneFill className='me-2' />
               +994 55 000 00 00
-              </div>
+            </div>
             <div className='d-flex align-items-center'>
               <AiOutlineMail className='me-2' />
               info@archbaku.az
-              </div>
+            </div>
             <div className='d-flex align-items-center'>
               <ImLocation className='me-2' />
               Bakı / Azərbaycan
-              </div>
+            </div>
           </div>
 
-          <div className="social">
-            social
+          <div className="d-flex align-items-center ">
+            <FaFacebookF />
+            <AiOutlineInstagram />
+            <AiFillLinkedin />
+            <AiOutlineWhatsApp />
           </div>
         </div>
       </div>
